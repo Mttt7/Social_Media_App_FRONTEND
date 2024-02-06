e782f9ca25e68b3834b790cd380ae90c71a65446
Michal Tomaszewski <groobytt@gmail.com>
2024-02-06 17:51:19 +0100

    improve getting posts from backend

--- socialMediaApp/src/app/components/home/home.component.ts
+++ socialMediaApp/src/app/components/home/home.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { PostService } from '../../services/post-service.service';
 import { Post } from '../../models/Post';
 import { ActivatedRoute } from '@angular/router';
-import { MatDialogRef } from '@angular/material/dialog';
-import { AddPostDialogComponent } from '../../dialogs/add-post-dialog/add-post-dialog.component';
 import { DialogService } from '../../services/dialog.service';
 
 @Component({
@@ -13,58 +11,63 @@ import { DialogService } from '../../services/dialog.service';
 })
 export class HomeComponent implements OnInit {
 
-
-
   posts: Post[] = [];
-  loading: boolean = false;
   pageNumber: number = 0;
+  mode: string = '';
+
+  loading: boolean = false;
   noMorePosts: boolean = false;
   refreshed: boolean = false;
+
+
 
   constructor(private postService: PostService, private route: ActivatedRoute,
     private dialogService: DialogService) { }
 
   ngOnInit(): void {
-    this.getPosts();
-  }
-
-  getPosts(loadmore: boolean = false) {
-
-
     this.route.paramMap.subscribe((params) => {
-      if (params.get('feedType') === 'feed') {
-        this.postService.getFeedPosts(this.pageNumber).subscribe(
-          data => {
-            if (data.last) this.noMorePosts = true
-
-            if (loadmore) this.posts = this.posts.concat(data.content as Post[])
-            else this.posts = data.content as Post[];
-
-            this.loading = false;
-          }
-        )
-      } else if (params.get('feedType') === 'user') {
-        this.postService.getUserPosts(this.pageNumber).subscribe(
-          data => {
-            if (data.last) this.noMorePosts = true;
-
-            if (loadmore) this.posts = this.posts.concat(data.content as Post[])
-            else this.posts = data.content as Post[];
-
-            this.loading = false;
-          }
-        )
-      }
+      this.pageNumber = 0;
+      this.noMorePosts = false;
+      this.posts = [];
+      this.mode = params.get('feedType')!;
+      this.getPosts(this.mode);
     })
   }
+
+  getPosts(feedType: string, loadmore: boolean = false) {
+    if (feedType === 'feed') {
+      this.postService.getFeedPosts(this.pageNumber).subscribe(
+        data => {
+          if (data.last) this.noMorePosts = true
+
+          if (loadmore) this.posts = this.posts.concat(data.content as Post[])
+          else this.posts = data.content as Post[];
+
+          this.loading = false;
+        }
+      )
+    } else if (feedType === 'user') {
+      this.postService.getUserPosts(this.pageNumber).subscribe(
+        data => {
+          if (data.last) this.noMorePosts = true;
+
+          if (loadmore) this.posts = this.posts.concat(data.content as Post[])
+          else this.posts = data.content as Post[];
+
+          this.loading = false;
+        }
+      )
+    }
+  }
+
   refresh() {
-    this.getPosts();
+    this.getPosts(this.mode);
   }
 
   loadMorePosts() {
     this.loading = true;
     this.pageNumber++;
-    this.getPosts(true);
+    this.getPosts(this.mode, true);
   }
 
   openDialog() {
@@ -75,19 +78,6 @@ export class HomeComponent implements OnInit {
       }
     )
   }
-
 }
 
-
-interface GetResponsePosts {
-  content: Post[];
-  pageable: any;
-  totalElements: number;
-  totalPages: number;
-  last: boolean;
-  size: number;
-  number: number;
-  first: boolean;
-  numberOfElements: number;
-}
 
